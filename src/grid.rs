dd3ed4e734eecda8641d3f554e83088617142eb6
Thegaming Life <thegaminglife09@gmail.com>
2023-02-23 17:54:48 +0100

    no bug but designs problems

--- src/grid.rs
+++ src/grid.rs
@@ -28,13 +28,13 @@ impl Grid {
     pub fn update(&self, screen: &mut [u8], vec: &Vec<Cell>) {
         
         let mut b = 0;
-        let mut color;
+        let mut color ;
 
         debug_assert_eq!(screen.len(), 4 * self.cells.len());
 
         for (_c, pix) in self.cells.iter().zip(screen.chunks_exact_mut(4)) {
             if b < 11950 && b > 10 {
-                if !vec[b].alive == true {
+                if !vec[b+1].alive == true {
                     color = [1, 0xF0, 0xF8, 0xFF];
                 } else {
                     color = [0x0, 0x0, 0x0, 0x0];
